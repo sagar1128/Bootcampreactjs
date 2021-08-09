817e4f77b8075433a1b0d11a65c71eae5271f367
sagar1128 <88628597+sagar1128@users.noreply.github.com>
2021-08-09 16:57:36 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App";
+
+// import Header from "./components/";
+ReactDOM.render(
+  <div>
+    <App />
+  </div>,
+  document.getElementById("root")
+);
